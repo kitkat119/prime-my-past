1085e4cf0856ba7aa38fe6bfa98b06650cf0e488
kitkat119 <gprydal@yahoo.co.uk>
2017-11-03 19:56:16 +0000

    tests for non-prime number

--- spec/primeFinderSpec.js
+++ spec/primeFinderSpec.js
@@ -8,10 +8,17 @@ describe('PrimeFinder', function() {
     primeFinder = new PrimeFinder();
     EXAMPLE_PRIME1 = 31;
     EXAMPLE_PRIME2 = 7;
+    EXAMPLE_NON_PRIME1 = 4;
+    EXAMPLE_NON_PRIME2 = 526;
   })
 
   it('outputs true if a number is prime', function() {
     expect(primeFinder.isPrime(EXAMPLE_PRIME1)).toEqual(true);
     expect(primeFinder.isPrime(EXAMPLE_PRIME2)).toEqual(true);
+  })
+
+  it('outputs false if a number is not prime', function() {
+    expect(primeFinder.isPrime(EXAMPLE_NON_PRIME1)).toBe(false);
+    expect(primeFinder.isPrime(EXAMPLE_NON_PRIME2)).toBe(false);
   })
 })
