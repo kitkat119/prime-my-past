fa5001c0952fd880e97f339eddfe7990cc737768
kitkat119 <gprydal@yahoo.co.uk>
2017-11-04 16:05:05 +0000

    file setup for multiplication table

---
+++ spec/multiplicationTableSpec.js
@@ -0,0 +1,9 @@
+const MultiplicationTable = require('../src/multiplicationTable').MultiplicationTable;
+
+describe ('MultiplicationTable', function() {
+  var table;
+
+  beforeEach(function() {
+    table = new MultiplicationTable();
+  })
+})
