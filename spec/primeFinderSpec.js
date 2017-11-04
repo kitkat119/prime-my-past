6eee07fd1c518f09a8454857d5afe8b50ab3671e
kitkat119 <gprydal@yahoo.co.uk>
2017-11-04 23:49:58 +0000

    refactoring primeFinder tests

--- spec/primeFinderSpec.js
+++ spec/primeFinderSpec.js
@@ -13,25 +13,35 @@ describe('PrimeFinder', function() {
 
   beforeEach(function() {
     primeFinder = new PrimeFinder();
-  })
-
-  it('outputs true if a number is prime', function() {
-    expect(primeFinder.isPrime(EXAMPLE_PRIME1)).toEqual(true);
-    expect(primeFinder.isPrime(EXAMPLE_PRIME2)).toEqual(true);
-  })
-
-  it('outputs false if a number is not prime', function() {
-    expect(primeFinder.isPrime(EXAMPLE_NON_PRIME1)).toBe(false);
-    expect(primeFinder.isPrime(EXAMPLE_NON_PRIME2)).toBe(false);
-    expect(primeFinder.isPrime(EXAMPLE_NON_PRIME_ONE)).toBe(false);
-    expect(primeFinder.isPrime(EXAMPLE_NON_PRIME_NEGATIVE)).toBe(false);
-  })
+  });
 
   it('has an array of primes', function() {
     expect(primeFinder.primes).toEqual([2]);
-  })
+  });
 
-  it("returns an array of n primes", function() {
-    expect(primeFinder.getPrimes(EXAMPLE_REQUEST)).toEqual([ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 ]);
-  })
-})
+  describe('isPrime', function() {
+    it('returns true if a number is prime', function() {
+      expect(primeFinder.isPrime(EXAMPLE_PRIME1)).toEqual(true);
+      expect(primeFinder.isPrime(EXAMPLE_PRIME2)).toEqual(true);
+    });
+
+    it('returns false for number 1', function() {
+      expect(primeFinder.isPrime(EXAMPLE_NON_PRIME_ONE)).toBe(false);
+    });
+
+    it('returns false for a negative number', function() {
+      expect(primeFinder.isPrime(EXAMPLE_NON_PRIME_NEGATIVE)).toBe(false);
+    });
+
+    it('returns false if a number is not prime', function() {
+      expect(primeFinder.isPrime(EXAMPLE_NON_PRIME1)).toBe(false);
+      expect(primeFinder.isPrime(EXAMPLE_NON_PRIME2)).toBe(false);
+    });
+  });
+
+  describe('getPrimes', function() {
+    it("returns an array of n primes", function() {
+      expect(primeFinder.getPrimes(EXAMPLE_REQUEST)).toEqual([ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 ]);
+    });
+  });
+});
