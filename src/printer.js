fccb46c122f3c45bca6af893f53151df40bd2c81
kitkat119 <gprydal@yahoo.co.uk>
2017-11-06 11:14:53 +0000

    working on table formatting

--- src/printer.js
+++ src/printer.js
@@ -1,8 +1,16 @@
 function Printer() {
 }
 Printer.prototype.printTable = function (data) {
-  data.forEach(function (row) {
+  var lastNumberInTable = data[data.length-1][data.length-1];
+  var sizeOfCell = lastNumberInTable.toString().length;
+  for(i=0; i < data.length; i ++) {
+    var row = data[i];
+    for(j = 0; j < row.length; j++) {
+      var extraLengthNeeded = sizeOfCell - row[j].toString().length;
+      var cell = row[j].toString();
+      cell + " ".repeat(extraLengthNeeded);
+    }
     console.log(row.join(" | "));
-  });
+  }
 };
 module.exports.Printer = Printer;
